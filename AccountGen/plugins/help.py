65e4d741319a2b4b50a6650c6b9bf8ba400d85dd
Devil <77043187+Devi-l007@users.noreply.github.com>
2021-03-21 15:29:23 +0530

    Update help.py

--- AccountGen/plugins/help.py
+++ AccountGen/plugins/help.py
@@ -13,10 +13,9 @@ from . import *
 
 @BotzHub.on(events.callbackquery.CallbackQuery(data="helpme"))
 async def helper(event):
-    text="I am an Account Generator Bot!\nI can generate working accounts for you.\n\nClick generate accounts to get your account!! Make sure to join my channel and support me!"
+    text="I am an Netflix Generator Bot!\nI generate working accounts for you.\n\nClick generate accounts to get your account!! Make sure to join my channel and support us!"
     await event.edit(text,
                      buttons=[
-                         [Button.url("Channel", url=ltc), Button.url("Dev", url="https://t.me/BotzHub")],
-                         [Button.url("Repository", url="https://github.com/xditya/AccountsGenBot")],
+                         [Button.url("Channel", url=ltc), Button.url("Dev", url="https://t.me/TheDynamicTricks")],
                          [Button.inline("Generate Accounts", data="gen")]
                      ])
